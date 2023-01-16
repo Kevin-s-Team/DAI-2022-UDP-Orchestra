d039dd02f6e2b02312a19f8bde3cba16dc9c544b
celestinPiccin <100125222+celestinPiccin@users.noreply.github.com>
2023-01-16 17:53:53 +0100

    musician

--- docker/image-musician/src/index.js
+++ docker/image-musician/src/index.js
@@ -1,1 +1,44 @@
-esrdfghj
+const dgram = require('dgram')
+const { argv } = require('process')
+const socket = dgram.createSocket('udp4')
+const { uuid } = require('uuidv4')
+const protocol = require('./musician-protocol')
+
+
+/*const multicast = {
+    multicastAddress: "231.6.7.9",
+    port: 8059
+}*/
+ 
+ const instrumentSounds = {
+     piano: 'ti-ta-ti',
+     trumpet: 'pouet',
+     flute: 'trulu',
+     violin: 'gzi-gzi',
+     drum: 'boum-boum'
+}
+
+if(process.argv.length != 3 || !(process.argv[2] in instrumentSounds)){
+    console.log('error with args');
+    process.exit(1);
+}
+
+const instrument = argv[2];
+const sound = instrumentSounds[instrument];
+const idMusician = uuid();
+
+const payload = JSON.stringify({
+    idMusician,
+    sound
+});
+
+setInterval( () => {
+    socket.send(payload, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS, (err, bytes) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("Sending payload: " + payload + " via port " + socket.address().port);
+        }
+    });
+}, protocol.INTERVAL);
---
+++ docker/image-musician/src/musician-protocol.js
@@ -0,0 +1,3 @@
+exports.PROTOCOL_MULTICAST_ADDRESS = "231.6.7.9";
+exports.PROTOCOL_PORT = 8059;
+exports.INTERVAL = 1000;
