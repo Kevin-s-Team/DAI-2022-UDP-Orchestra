5b7b7fcfeb13f484e54453dd563927ef1b6109c9
celestinPiccin <100125222+celestinPiccin@users.noreply.github.com>
2023-01-27 18:32:47 +0100

    comment + nettoyage

--- docker/image-auditor/src/index.js
+++ docker/image-auditor/src/index.js
@@ -1,10 +1,12 @@
 const dgram = require('dgram')
-const { argv } = require('process')
 const net = require('net')
-const protocol = require('./auditor-protocol')
 const day = require('dayjs')
+const protocol = require('./auditor-protocol')
 
-const socket = dgram.createSocket('udp4')
+// crée un socket UDP IPv4
+const socket = dgram.createSocket('udp4');
+
+// crée un serveur TCP
 const server = net.createServer(onClientConnection);
 
 const musicians = new Map();
@@ -18,14 +20,20 @@ const soundsIntstruments = {
 }
 
 
+/* partie UDP */
+
+// écoute sur le port 8059 du groupe multicast
 socket.bind(protocol.PROTOCOL_PORT, () => {
     console.log("Joining multicast group");
     socket.addMembership(protocol.PROTOCOL_MULTICAST_ADDRESS);
 });
 
+// appelle la fonction lambda suivante lorsqu'on reçoit un event de type message
 socket.on('message', (msg, rinfo) => {
+    // récupération des données du message
     const { idMusician, sound } = JSON.parse(msg.toString());
 
+    // si le musicien n'est pas dans la Map on l'ajoute dans la Map
     if(!musicians.has(idMusician)){
         const instrument = soundsIntstruments[sound];
         const activeSince = day();
@@ -34,14 +42,15 @@ socket.on('message', (msg, rinfo) => {
         console.log(musicians.get(idMusician));
 
     }
-    else{
+    else{ // sinon on met a jour la propriété lastPlay avec le temps actuel
         var musician = musicians.get(idMusician);
         musicians.set(idMusician, {...musician, lastPlay:day()});
     }
-
 });
 
 
+// fonction qui contrôle si un musicien à émis un son les 5 dernières sec,
+// si ce n'est pas le cas il est enlevé de la Map
 function checkActive(){
     for(let key of musicians.keys()){
         var musician = musicians.get(key);
@@ -51,23 +60,30 @@ function checkActive(){
     }
 }
 
+// la fonction de contrôle est exécutée toutes les 1/2 sec
 setInterval(checkActive, 500);
 
+
+/* partie TCP */
+
+// le serveur TCP écoute sur le port 2205
 server.listen(protocol.TCP_PORT, () => {
     console.log('Server started on port '+ protocol.TCP_PORT)
 });
 
-
+// fonction de callback lorsqu'un client se connecte
 function onClientConnection(sock){
-    console.log("new connection established");
-        
+    console.log("New connection established");
+      
+    // préparation du message à envoyé
     payload = [];
     for (const val of musicians.values()) {
         let res = {...val};
         delete res.lastPlay;
-        console.log(res);
         payload.push(res);
     }
+
+    // envoie sur le socket puis termine la connexion
     sock.write(JSON.stringify(payload));
     sock.end();
 };
--- docker/image-musician/src/index.js
+++ docker/image-musician/src/index.js
@@ -1,25 +1,22 @@
 const dgram = require('node:dgram')
 const { argv } = require('process')
-const socket = dgram.createSocket('udp4')
 const { v4: uuid } = require('uuid')
 const protocol = require('./musician-protocol')
 
-
-/*const multicast = {
-    multicastAddress: "231.6.7.9",
-    port: 8059
-}*/
-
 const instrumentSounds = {
-     piano: 'ti-ta-ti',
-     trumpet: 'pouet',
-     flute: 'trulu',
-     violin: 'gzi-gzi',
-     drum: 'boum-boum'
+    piano: 'ti-ta-ti',
+    trumpet: 'pouet',
+    flute: 'trulu',
+    violin: 'gzi-gzi',
+    drum: 'boum-boum'
 }
 
+// crée un socket UDP IPv4
+const socket = dgram.createSocket('udp4')
+
+// contrôle des arguments
 if(argv.length != 3 || !(argv[2] in instrumentSounds)){
-    console.log('error with args');
+    console.log('Error with args');
     process.exit(1);
 }
 
@@ -27,13 +24,18 @@ const instrument = argv[2];
 const sound = instrumentSounds[instrument];
 const idMusician = uuid();
 
+// prépare le payload sous format JSON
 const payload = JSON.stringify({
     idMusician,
     sound
 });
 
+// execute la fonction (lambda) suivante toute les 1000 ms (1s)
 setInterval( () => {
-    socket.send(payload, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS, (err, bytes) => {
+
+    // envoie du payload sur le port 8059 et l'IP multicast 231.6.7.9
+    socket.send(payload, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS, (err) => {
+        // callback appelé après que le message à été envoyé
         if(err){
             console.log(err);
         }
